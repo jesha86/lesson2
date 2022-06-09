cb88d4d0f473ff97b6f215ea0d01024fa6e72c61
Jesha86 <jeshakhafizova@gmail.com>
2022-06-09 16:24:06 +0200

    Java Core Homework2

---
+++ src/main/java/ru/geekbrains/javacore2/MyArrayDataException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.javacore2;
+
+public class MyArrayDataException extends Exception {
+    public MyArrayDataException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/geekbrains/javacore2/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.javacore2;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
+
